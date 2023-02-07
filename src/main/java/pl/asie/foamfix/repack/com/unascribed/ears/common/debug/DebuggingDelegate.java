ef09c29084bbe403d8c6d6b8079ebf70dc378fc5
Adrian Siekierka <kontakt@asie.pl>
2023-02-07 06:50:36 +0100

    fix .gitignore

---
+++ src/main/java/pl/asie/foamfix/repack/com/unascribed/ears/common/debug/DebuggingDelegate.java
@@ -0,0 +1,245 @@
+package pl.asie.foamfix.repack.com.unascribed.ears.common.debug;
+
+import pl.asie.foamfix.repack.com.unascribed.ears.common.render.EarsRenderDelegate;
+
+/**
+ * A wrapper around another EarsRenderDelegate that {@link EarsLog logs} everything performed.
+ */
+public class DebuggingDelegate implements EarsRenderDelegate {
+
+	private final EarsRenderDelegate delegate;
+
+	public DebuggingDelegate(EarsRenderDelegate delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public void push() {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "push()");
+		delegate.push();
+	}
+
+	@Override
+	public void pop() {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "pop()");
+		delegate.pop();
+	}
+
+	@Override
+	public void anchorTo(BodyPart part) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "anchorTo(BodyPart.{})", part);
+		delegate.anchorTo(part);
+	}
+
+	@Override
+	public void translate(float x, float y, float z) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "translate({}, {}, {})", x, y, z);
+		delegate.translate(x, y, z);
+	}
+
+	@Override
+	public void rotate(float ang, float x, float y, float z) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "rotate({}, {}, {}, {})", ang, x, y, z);
+		delegate.rotate(ang, x, y, z);
+	}
+
+	@Override
+	public void renderFront(int u, int v, int w, int h, TexRotation rot, TexFlip flip, QuadGrow grow) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "renderFront({}, {}, {}, {}, {}, {}, {})", u, v, w, h, rot, flip, grow);
+		delegate.renderFront(u, v, w, h, rot, flip, grow);
+	}
+
+	@Override
+	public void renderBack(int u, int v, int w, int h, TexRotation rot, TexFlip flip, QuadGrow grow) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "renderBack({}, {}, {}, {}, {}, {}, {})", u, v, w, h, rot, flip, grow);
+		delegate.renderBack(u, v, w, h, rot, flip, grow);
+	}
+	
+	@Override
+	public void renderDoubleSided(int u, int v, int w, int h, TexRotation rot, TexFlip flip, QuadGrow grow) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "renderDoubleSided({}, {}, {}, {}, {}, {}, {})", u, v, w, h, rot, flip, grow);
+		delegate.renderDoubleSided(u, v, w, h, rot, flip, grow);
+	}
+
+	@Override
+	public void renderDebugDot(float r, float g, float b, float a) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "renderDebugDot({}, {}, {}, {})", r, g, b, a);
+		delegate.renderDebugDot(r, g, b, a);
+	}
+
+	@Override
+	public void bind(TexSource tex) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "bind({})", tex);
+		delegate.bind(tex);
+	}
+
+	@Override
+	public void scale(float x, float y, float z) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "scale({}, {}, {})", x, y, z);
+		delegate.scale(x, y, z);
+	}
+
+	@Override
+	public void setUp() {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "setUp()");
+		delegate.setUp();
+	}
+
+	@Override
+	public void tearDown() {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "tearDown()");
+		delegate.tearDown();
+	}
+
+	@Override
+	public float getTime() {
+		float t = delegate.getTime();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getTime() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public boolean isFlying() {
+		boolean b = delegate.isFlying();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "isFlying() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public boolean isGliding() {
+		boolean b = delegate.isGliding();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "isGliding() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public boolean isWearingElytra() {
+		boolean b = delegate.isWearingElytra();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "isWearingElytra() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public boolean isWearingChestplate() {
+		boolean b = delegate.isWearingChestplate();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "isWearingChestplate() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public boolean isWearingBoots() {
+		boolean b = delegate.isWearingBoots();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "isWearingBoots() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public boolean isJacketEnabled() {
+		boolean b = delegate.isJacketEnabled();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "isJacketEnabled() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public Object getPeer() {
+		return delegate.getPeer();
+	}
+	
+	@Override
+	public boolean needsSecondaryLayersDrawn() {
+		boolean b = delegate.needsSecondaryLayersDrawn();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "needsSecondaryLayersDrawn() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public float getLimbSwing() {
+		float t = delegate.getLimbSwing();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getLimbSwing() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public float getHorizontalSpeed() {
+		float t = delegate.getHorizontalSpeed();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getHorizontalSpeed() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public float getStride() {
+		float t = delegate.getStride();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getStride() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public boolean isSlim() {
+		boolean b = delegate.isSlim();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "isSlim() -> {}", b);
+		return b;
+	}
+
+	@Override
+	public float getBodyYaw() {
+		float t = delegate.getBodyYaw();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getBodyYaw() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public double getX() {
+		double t = delegate.getX();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getX() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public double getY() {
+		double t = delegate.getY();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getY() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public double getZ() {
+		double t = delegate.getZ();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getZ() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public double getCapeX() {
+		double t = delegate.getCapeX();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getCapeX() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public double getCapeY() {
+		double t = delegate.getCapeY();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getCapeY() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public double getCapeZ() {
+		double t = delegate.getCapeZ();
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "getCapeZ() -> {}", t);
+		return t;
+	}
+
+	@Override
+	public void setEmissive(boolean emissive) {
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "setEmissive({})", emissive);
+		delegate.setEmissive(emissive);
+	}
+	
+	@Override
+	public boolean canBind(TexSource tex) {
+		boolean b = delegate.canBind(tex);
+		EarsLog.debug(EarsLog.Tag.PLATFORM_RENDERER_DELEGATE, "canBind({}) -> {}", tex, b);
+		return b;
+	}
+	
+}
---
+++ src/main/java/pl/asie/foamfix/repack/com/unascribed/ears/common/debug/EarsLog.java
@@ -0,0 +1,260 @@
+package pl.asie.foamfix.repack.com.unascribed.ears.common.debug;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EarsLog {
+	
+	public static class Tag {
+		
+		private static final Set<String> ALL = new HashSet<String>();
+		
+		/** Nonspecific common code, ignores ONLY_DEBUG */
+		public static final Tag COMMON_ALWAYS = new Tag("Common");
+		
+		/** Nonspecific common code */
+		public static final Tag COMMON = new Tag("Common");
+		/** Common code related to agent patching */
+		public static final Tag COMMON_AGENT = new Tag("Common:Agent");
+		/** Common code related to the API */
+		public static final Tag COMMON_API = new Tag("Common:Api");
+		/** Common code related to feature detection */
+		public static final Tag COMMON_FEATURES = new Tag("Common:Features");
+		/** Common code related to mixin patching */
+		public static final Tag COMMON_MIXIN = new Tag("Common:Mixin");
+		/** Common code related to rendering */
+		public static final Tag COMMON_RENDERER = new Tag("Common:Renderer");
+		/** Dummy tag to enable debug dots in the renderer */
+		public static final Tag COMMON_RENDERER_DOTS = new Tag("Common:Renderer:Dots");
+		
+		/** Nonspecific platform code */
+		public static final Tag PLATFORM = new Tag("Platform");
+		/** Platform code related to loading images */
+		public static final Tag PLATFORM_LOAD = new Tag("Platform:Load");
+		/** Platform code related to rendering and render hooking */
+		public static final Tag PLATFORM_RENDERER = new Tag("Platform:Renderer");
+		/** Platform code related to implementing the common render delegate */
+		public static final Tag PLATFORM_RENDERER_DELEGATE = new Tag("Platform:Renderer:Delegate");
+		/** Platform code relating to patches and injections */
+		public static final Tag PLATFORM_INJECT = new Tag("Platform:Inject");
+		/** Platform code relating to injected render hooks */
+		public static final Tag PLATFORM_INJECT_RENDERER = new Tag("Platform:Inject:Renderer");
+		
+		private final String tag;
+		
+		private Tag(String tag) {
+			this.tag = tag;
+			ALL.add(tag);
+		}
+		
+		@Override
+		public String toString() {
+			return tag;
+		}
+	}
+
+	public static final boolean DEBUG = false;
+
+	static {
+	}
+
+	/**
+	 * @return {@code true} if debugging and the given debug tag are enabled
+	 */
+	@SuppressWarnings("unused")
+	public static boolean shouldLog(Tag tag) {
+		return false;
+	}
+	
+	/**
+	 * Print a log message to the Ears log file (or stdout, if configured).
+	 * <p>
+	 * Prefer one of the many specializations of this method that don't allocate an array; Ears
+	 * tends to inject into hot paths in the Minecraft renderer, so it's good to avoid the array
+	 * allocation if debugging is off.
+	 */
+	public static void debugva(Tag tag, String fmt, Object... arg) {
+
+	}
+	
+	public static void debug(Tag tag, String fmt) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, Object arg1) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, float arg1) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, double arg1) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, boolean arg1) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, int arg1) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, int arg1, int arg2) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, int arg1, Object arg2) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, Object arg2) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, float arg2) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, Object arg1, Object arg2, float arg3) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, int arg1, Object arg2, boolean arg3) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, Object arg2, Object arg3) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, int arg1, Object arg2, Object arg3) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, float arg2, float arg3) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, float arg1, float arg2, float arg3) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, int arg1, int arg2, Object arg3) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, float arg1, float arg2, float arg3, float arg4) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, float arg2, float arg3, float arg4) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, Object arg2, Object arg3, Object arg4) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, Object arg1, float arg2, float arg3, float arg4, float arg5) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, int arg2, int arg3, int arg4, int arg5) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, int arg1, int arg2, int arg3, int arg4, boolean arg5) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, int arg2, int arg3, int arg4, int arg5, boolean arg6) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5, arg6);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, Object arg1, Object arg2, int arg3, int arg4, int arg5, int arg6) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5, arg6);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, int arg1, int arg2, int arg3, int arg4, Object arg5, Object arg6) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5, arg6);
+		}
+	}
+	
+	public static void debug(Tag tag, String fmt, int arg1, int arg2, int arg3, int arg4, Object arg5, Object arg6, Object arg7) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, Object arg1, float arg2, float arg3, float arg4, float arg5, float arg6, float arg7, float arg8) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+		}
+	}
+
+	public static void debug(Tag tag, String fmt, Object arg1, Object arg2, int arg3, Object arg4, float arg5, float arg6, float arg7, float arg8, float arg9, float arg10) {
+		if (shouldLog(tag)) {
+			debugva(tag, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+		}
+	}
+
+}
